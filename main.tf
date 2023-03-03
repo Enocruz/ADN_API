3891790b05a7e8aa4d2b2418ef230bd78b6c5c0a
bcruz <b-andon@hotmail.com>
2023-03-02 20:00:05 -0600

    Terraform deployment

---
+++ main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "archive_file" "lambda_adn" {
+  type = "zip"
+
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/lambda_adn.zip"
+}
+
+# to Create function
+resource "aws_lambda_function" "lambda_adn" {
+  function_name    = "lambda_adn"
+  filename         = "lambda_adn.zip"
+  runtime          = "python3.9"
+  handler          = "index.handler"
+  source_code_hash = data.archive_file.lambda_adn.output_base64sha256
+  role             = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "lambda_adn" {
+  name              = "/aws/lambda/lambda_adn"
+  retention_in_days = 3
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
